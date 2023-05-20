ecb6088fe95a1e1694fd89e1c3c3ea115951f48e
LightmanBro <omotosodavidromeo@gmail.com>
2023-05-20 09:01:14 +0100

    Solved the Dom manipulation of the trade posts.

--- payment gateway/trade/script.js
+++ payment gateway/trade/script.js
@@ -1,6 +1,10 @@
 const buy_tableBody = document.querySelector(".buy_other_methods"),
   sell_tableBody = document.querySelector(".sell_other_methods"),
   popContainer = document.querySelector("popup-container4");
+
+
+var userID;
+const retrieved = []
 // console.log(document.querySelector(".buy_other_methods").innerHTML)
 function loadData(method, url) {
   //Use javascript to set the attribute of the form, both the me
@@ -10,13 +14,15 @@ function loadData(method, url) {
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.onload = function () {
     const datas = JSON.parse(this.response);
+    retrieved.push(datas);
     datas.forEach((data) => {
       const html = `
       <tr>
         <td>
+        <input type="tel" placeholder=" $" value="${data.user_id}" hidden>
           <div class="card-info1">
 
-            <h3> ${data.user_name}-- Id<span>${data.user_id} </span></h3>
+            <h3> ${data.user_name}-- Id<span> ${data.user_id} </span></h3>
 
           </div>
         </td>
@@ -42,191 +48,147 @@ function loadData(method, url) {
           </div>
         </td>
         <td>
-          <div class="methods">
+        <div class="methods">
             <span>${data.payment_method_1},</span>
             <span>${data.payment_method_2},</span>
             <span>${data.payment_method_3},</span>
           </div>
         </td>
-        <td>
-          <div class="card-buy">
-            <button class="btn4" id="modal-btn"> Buy NGN</button>
+        <td class="flex">
+              <td>
+                <div class="card-buy">
+                  <button class="btn4" id="modal-btn"> Buy ${data.wallet}</button>
+                </div>
+              </td>
+          <div class="methods">
+          <div class="inline">
+                <button class="btn4" id="modal-btn"> <span>${data.payment_method_1}</span></button>
+                <button class="btn4" id="modal-btn"><span>${data.payment_method_2}</span></button>
+                <button class="btn4" id="modal-btn"> <span>${data.payment_method_3}</span></button>
+
+                <div class="floating_container none" id="floating_container">
+                <span>${data.payment_method_1},</span>
+                <span>${data.payment_method_2},</span>
+                <span>${data.payment_method_3},</span>
+            <div class="payment_receiving_methods">
+              <form  id="bank_details">
+                <div class="payment-method-bank" id="bank-box">
+                  <div class="name-details">
+                    <h4>BANK NAME:</h4>
+                    <p>SPRING BANK</p>
+                  </div>
+                  <div class="name-details">
+                    <h4>ACCNT NAME:</h4>
+                    <p>ADEJUYI SAMSON J</p>
+                  </div>
+                  <div class="name-details">
+                    <h4>ACCOUNT NO:</h4>
+                    <p>2187093564</p>
+                  </div>
+                  <div class="name-details">
+                    <h4>PURCHASE AMOUNT</h4><input type="tel" placeholder=" $">
+                  </div>
+                  <div class="name-details">
+                    <h4>PURCHASE COST:</h4>
+                    <p>&#x20A6;74,000</p>
+                  </div>
+                  <div class="name-details">
+                    <h4>FEE 1%:</h4>
+                    <p>$1</p>
+                  </div>
+                  <div class="name-details">
+                    <h4>RECEIVE AMOUNT:</h4>
+                    <p>$99.00</p>
+                  </div>
+                  <div class="btn-proceed">
+
+                    <a href="verify.html">
+                      <button class="close-btn4" id="cancel-inner"> CONFIRM</button>
+                    </a>
+
+                    <button class="close-btn5" id="cancel" style="background-color: var(--secondary);">CANCEL</button>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-        </td>
-      </tr>`;
 
-      // for the modal box of each container
-
+        </div>
+        </div>
+      </td>
+        
+    </tr>`;
       buy_tableBody.innerHTML += html;
 
-      const userId = buy_tableBody.querySelector("tr td .card-info1 span");
-      const options = { method: "POST", body: datas.user_id }; //optional data for the fetch api to send to the database.
-      const paymentModalBank = document.querySelector('.transaction form[id="bank_details"]')
-      const paymentModalAple = document.querySelector('.transaction form[id="apple_details"]')
-      
-      const url = "../php/payment-options.php";
-
-      // Adding the available payment methods to the select list.
-      const modalContainer = document.querySelector('.transaction');
 
 
 
-
-
-
+      const btn = buy_tableBody.querySelectorAll('tr td .methods .inline button');
+       
       // getting the element sent to the dom inside the dom and calling an eventListener on each of them.
+      btn.forEach(buttons=>{
+        buttons.addEventListener(click,(e)=>{
+          e.preventDefault();
+          console.log(buy_tableBody.querySelectorAll('tr td .methods .inline #floating_container'))
+        })
+      })
       buy_tableBody.querySelectorAll("tr").forEach((child) => {
         child.lastElementChild
           .querySelector(".card-buy button")
           .addEventListener("click", (e) => {
-            console.log(userId.innerHTML * 1);
+            console.log('working button')
+            const floatingContainer = document.querySelector('#floating_container');
+            floatingContainer.classList.toggle('none');
+            // floatingContainer.innerHTML = 'Good Morning 😂';
 
-            fetch(url, options)
-              .then((response) => response.json())
-              .then(paymentdata =>{
-                paymentdata.forEach(paymentD=>{
-                  // console.log(paymentdata);
-                  // console.log(paymentModal.nextElementSibling);
-                  if (paymentD["bank_name"]) {
-                    const bankHtml = ` 
-                          <div class="payment-method-bank" id="bank-box">
-                            <div class="name-details">
-                              <h4>BANK NAME:</h4>
-                              <p>${paymentD["bank_name"].toUpperCase()}</p>
-                            </div>
-                            <div class="name-details">
-                              <h4>ACCNT NAME:</h4>
-                              <p>${paymentD["user_name"]}</p>
-                            </div>
-                            <div class="name-details">
-                              <h4>ACCOUNT NO:</h4>
-                              <p>${paymentD["account_number"]}</p>
-                            </div>
-                            <div class="name-details">
-                              <h4>PURCHASE AMOUNT</h4><input type="tel" placeholder=" $">
-                            </div>
-                            <div class="name-details">
-                              <h4>PURCHASE COST:</h4>
-                              <p>${data.user_rate * data.highest_rate}</p>
-                            </div>
-                            <div class="name-details">
-                              <h4>FEE 1%:</h4>
-                              <p>${data.user_rate * data.highest_rate * 0.01}</p>
-                            </div>
-                            <div class="name-details">
-                              <h4>RECEIVE AMOUNT:</h4>
-                              <p>${(data.user_rate * data.highest_rate) - (data.user_rate * data.highest_rate * 0.01)}</p>
-                            </div>
-                            <div class="btn-proceed">
-                              <a href="verify.html">
-                                <button type="reset" class="close-btn4" id="cancel-inner"> CONFIRM</button>
-                              </a>
-                              <button type="submit" class="close-btn5" id="cancel" style="background-color: var(--secondary);">CANCEL</button>
-                            </div>
-                          </div>
-                     `
-                    // paymentModalBank.innerHTML= bankHtml
-                  } else if ("pi") {
-                    `<form >
-                  <div class="pi-box" id="pi-box" style="display: none;">
-                    <div class="name-details">
-                      <h4>METHOD</h4>
-                      <p>PI NETWORK</p>
-                    </div>
-                    <div class="name-details">
-                      <h4>WALLET ADDRESS:</h4>
-                      <p>#2187093564</p>
-                    </div>
-                    <div class="name-details">
-                        <h4>PURCHASE AMOUNT</h4> <input type="tel" placeholder="" name="amount">
-                      </div>
-                    <div class="name-details">
-                      <h4>PURCHASE COST:</h4>
-                      <p>${data.user_rate * data.highest_rate}</p>
-                    </div>
-                    <div class="name-details">
-                      <h4>FEE 1%:</h4>
-                      <p>${data.user_rate * data.highest_rate * 0.01}</p>
-                    </div>
-                    <div class="name-details">
-                      <h4>RECEIVE AMOUNT:</h4>
-                      <p>${(data.user_rate * data.highest_rate) - (data.user_rate * data.highest_rate * 0.1)}</p>
-                    </div>
-                    <div class="btn-proceed">
-                    <a href="verify.html">
-                      <button type="reset" class="close-btn4" id="cancel-inner"> CONFIRM</button>
-                    </a>
-                    <button type="submit" class="close-btn5" id="cancel" style="background-color: var(--secondary);">CANCEL</button>
-                  </div>
-                  </div>
-                </form>`;
-                  } else if ("google") {
-                  } else if ("skrill") {
-                  } else if (paymentD["apple_email"]) {
-                    const appleHtml=
-                    `<div class="pi-box" id="pi-box" style="display: none;">
-                      <div class="name-details">
-                        <h4>METHOD</h4>
-                        <p>CHIPPER CASH</p>
-                      </div>
-                      <div class="name-details">
-                        <h4> USERNAME:</h4>
-                        <p>${paymentD["apple_email"]}</p>
-                      </div>
-                      <div class="name-details">
-                        <h4>PURCHASE AMOUNT</h4> <input type="tel" placeholder="" name="amount">
-                      </div>
-                      <div class="name-details">
-                        <h4>PURCHASE COST:</h4>
-                        <p>${data.user_rate * data.highest_rate}</p>
-                      </div>
-                      <div class="name-details">
-                        <h4>FEE 1%:</h4>
-                        <p>${data.user_rate * data.highest_rate * 0.1}</p>
-                      </div>
-                      <div class="name-details">
-                        <h4>RECEIVE AMOUNT:</h4>
-                        <p>${(data.user_rate * data.highest_rate) - (data.user_rate * data.highest_rate * 0.1)}</p>
-                      </div>
-                      <div class="btn-proceed">
-                        <a href="verify.html">
-                          <button type="reset" class="close-btn4" id="cancel-inner"> CONFIRM</button>
-                        </a>
-                          <button type="submit" class="close-btn5" id="cancel" style="background-color: var(--secondary);">CANCEL</button>
-                      </div>
-                    </div>`
-                    paymentModalAple.innerHTML =appleHtml;
-                  } else if ("momo") {
-                  } else if ("paypal") {
-                  } else if ("") {
-                  }
-  
-                  
-                })
-              }
-                
-                //Map the response to the modal box
-
-                //Then insert the modal box data into the modal box.
-              )
-              .catch((err) => console.error(err));
-
-            //this function is gotten from main.js script.
-            displayPopup4();
+            // retrieved.forEach(retrieve=>{
+            //   retrieve.forEach(element => {
+            //     console.log(element)
+            //   });
+            // })
+            // displayPopup4();
           });
       });
     });
+    
   };
-  // .lastElementChild.querySelector('.card-buy button').addEventListener('click',(e)=>{
-  //   e.preventDefault();
-  //   console.log('button clicked')
-  // })
+////////////////////////////////////////////
+  function handleclick(event){
+    var item = event.target;
+    if(item.tagName === "BUTTON"){
+      console.log('Button clicked')
+      //method to work on individual container clicked
+      var userID = item.parentElement.parentElement.parentElement.querySelector('td input').value;
+      var paymentMethod1 = item.parentElement.parentElement.parentElement.querySelectorAll('td .methods span');
+      var userName = item.parentElement.parentElement.parentElement.querySelector('td .card-info1 h3');
+      var paymentModal = item.parentElement.parentElement.parentElement.querySelector('td ')
+      console.log(userID *1);
+      console.log(paymentMethod1[0].innerHTML,paymentMethod1[1].innerHTML,paymentMethod1[2].innerHTML);
+      console.log(userName);
+      console.log(paymentModal);
+    }else{
+      console.log('Not a button')
+    }
+  }
+  buy_tableBody.addEventListener('click',(e)=>{
+    e.stopPropagation();
+    handleclick(e);
+    if(e.target.tagName === "BUTTON"){
+       console.log('button clicked from the second function')
+    }else{
+      console.log('not a button')
+    }
+  })
+////////////////////////////////////
   xhr.send();
 }
-
+////////////////////////////////////
 const method_buy = "POST",
   url_buy = "../php/order_buy.php";
 loadData(method_buy, url_buy);
+
+
+/////////////////////////////////////
 
 function loadData2(method, url) {
   const xhr = new XMLHttpRequest();
@@ -290,15 +252,7 @@ const method_sell = "POST",
   url_sell = "../php/order_sell.php";
 loadData2(method_sell, url_sell);
 
-async function fetchAsync() {
-  let response = await fetch("../php/payment-options.php");
-  let data = await response.json();
-  console.log(data);
-  return data;
-}
-
-fetchAsync();
-
+console.log(userID)
 `              <div class="transaction">
 <div class="payment-method-head">
   <H4>OTHER PAYMENT METHODS</H4>
@@ -405,4 +359,4 @@ fetchAsync();
     <button class="close-btn4" id="cancel" style="background-color: var(--secondary);">CANCEL</button>
   </div>
 </div>
-</div>`;
+</div>`
