df8f1ade39a944c5629e26936ed22264c40bf7d3
LightmanBro <omotosodavidromeo@gmail.com>
2023-05-24 14:35:47 +0100

    fixed the buy trade modal box.

--- payment gateway/trade/safety.js
+++ payment gateway/trade/safety.js
@@ -1,45 +1,7 @@
 // const userId = buy_tableBody.querySelector(' tr td .card-info1 span');
-document.addEventListener('DOMContentLoaded',()=>{
-  buy_tableBod = document.querySelector(".buy_other_methods");
-  var userI =  buy_tableBod.querySelectorAll(
-    " tr td input"
-  );
-  buy_tableBod.querySelectorAll("tr").forEach((child) => {child.lastElementChild.querySelector(".card-buy button").addEventListener("click", (e) => {
-        //Geting the inner html which will serve as the poster
-        console.log(userI);
-        // const url = "../php/payment-receiving-methods.php";
-        // const xmlhttp = new XMLHttpRequest();
-        // xmlhttp.open("POST", url); //this can make us send both request and response on thesame page
-        // xmlhttp.setRequestHeader(
-        //   "Content-Type",
-        //   "application/x-www-form-urlencoded"
-        // );
-        // xmlhttp.onload = () => {
-        //   const paymentdata = JSON.parse(this.responseText);
-        //   console.log(paymentdata);
-  
-        //   paymentdata.forEach((paymentD) => {
-        //     console.log(paymentdata);
-        //     console.log(paymentD);
-  
-        //   });
-        // };
-        // // const urlEncodedData = new URLSearchParams(formData).toString();
-        // console.log(data.user_id);
-        // xmlhttp.send(userI.innerHTML * 1);
-        displayPopup4();
-      });
-    });
-    
-  console.log(userI.value);
-  console.log(buy_tableBody);
-})
-
-async function fetchAsync() {
-  let response = await fetch("../php/payment-options.php");
+export async function fetchAsync(url) {
+  let response = await fetch(url);
   let data = await response.json();
   console.log(data);
   return data;
-}
-
-fetchAsync();
+};
--- payment gateway/trade/script.js
+++ payment gateway/trade/script.js
@@ -1,10 +1,12 @@
+import { fetchAsync } from "./safety.js";
+
 const buy_tableBody = document.querySelector(".buy_other_methods"),
   sell_tableBody = document.querySelector(".sell_other_methods"),
   popContainer = document.querySelector("popup-container4");
 
 
-var userID;
-const retrieved = []
+
+const retrieved = {};
 // console.log(document.querySelector(".buy_other_methods").innerHTML)
 function loadData(method, url) {
   //Use javascript to set the attribute of the form, both the me
@@ -12,45 +14,56 @@ function loadData(method, url) {
   const xhr = new XMLHttpRequest();
   xhr.open(method, url);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  //The floating container form.
   const html2 = `
   <div class="floating_container" id="floating_container">
+  
+  <div class="sp">
+  <span>Payment receiving methods</span>
+  <hr>
+  <p></p>
   <!-- Receiving Acounts available will be shown here in this span-->
-  <span> </span>
-  <span> </span>
-  <span> </span>
+  
+  </div>
   <hr>
 <div class="payment_receiving_methods">
-  <h3> </span></h3>
+  <h3> </h3>
   <hr>
   <form id="bank_details">
       <div class="payment-method-bank" id="bank-box">
       <div class="name-details">
-      <h4>BANK NAME:</h4>
+      <h4>Bank name:</h4>
       <p>SPRING BANK</p>
       </div>
       <input type="text" placeholder="" name="wallet" hidden>
+      <input type="text" placeholder="" name="user_id" hidden>
         <div class="name-details">
-          <h4>ACCNT NAME:</h4>
+          <h4>Account name:</h4>
           <p>ADEJUYI SAMSON J</p>
         </div>
         <div class="name-details">
-          <h4>ACCOUNT NO:</h4>
+          <h4>Account number:</h4>
           <p>2187093564</p>
         </div>
+        <hr>
         <div class="name-details" id="amount">
-          <h4>PURCHASE AMOUNT</h4><input type="text" placeholder="" name="buy_amount">
+          <h4>Order unit</h4><input type="text" placeholder="" name="buy_amount">
         </div>
-        <div class="name-details">
-          <h4>PURCHASE COST:</h4>
-          <p>&#x20A6;74,000</p>
+        <div class="name-details order_cost">
+          <h4>Order cost:</h4>
+          <p></p>
         </div>
-        <div class="name-details">
-          <h4>FEE 1%:</h4>
-          <p>$1</p>
+        <div class="name-details rate">
+          <h4>Curr Exchange rate:</h4>
+          <p></p>
         </div>
-        <div class="name-details">
-          <h4>RECEIVE AMOUNT:</h4>
-          <p>$99.00</p>
+        <div class="name-details fee">
+          <h4>Transaction fee 1%:</h4>
+          <p> </p>
+        </div>
+        <div class="name-details receive">
+          <h4>Receive:</h4>
+          <p>00</p>
         </div>
         <div class="btn-proceed" id="proceed">
 
@@ -63,12 +76,14 @@ function loadData(method, url) {
 </form>
 </div>
 </div>
-  `
+  `;
+
   xhr.onload = function () {
     const datas = JSON.parse(this.response);
-    retrieved.push(datas);
+    retrieved.data = datas;
+
     datas.forEach((data) => {
-    const html=`<tr>
+      const html = `<tr>
         <td>
         <input type="tel" placeholder=" $" value="${data.user_id}" hidden>
           <div class="card-info1">
@@ -119,45 +134,85 @@ function loadData(method, url) {
       buy_tableBody.innerHTML += html;
     });
   };
-/////////////////The function that handles the click on individual container///////////////////////////
-  function handleclick(event){
+  /////////////////The function that handles the click on individual container///////////////////////////
+  function handleclick(event) {
     var item = event.target;
-    if(item.tagName === "BUTTON"){
-      document.querySelector('.popup-container4').innerHTML = html2;
+    if (item.tagName === "BUTTON") {
+      document.querySelector(".popup-container4").innerHTML = html2;
       // console.log('Button clicked')
       //method to work on individual container clicked
-      var userID = item.parentElement.parentElement.parentElement.querySelector('td input');
-      var paymentMethod1 = item.parentElement.parentElement.parentElement.querySelectorAll('td .methods span');
-      var userName = item.parentElement.parentElement.parentElement.querySelector('td .card-info1 h3');
-      var wallet = item.parentElement.parentElement.parentElement.querySelector('td .card-info2 .wallet span');
-      var purchaseAmnt = document.querySelector(".payment-method-bank #amount input");
-      var proceedBtn = document.querySelector('.payment-method-bank #proceed a')
+      const userID =
+        item.parentElement.parentElement.parentElement.querySelector(
+          "td input"
+        );
+      const paymentMethod1 =
+        item.parentElement.parentElement.parentElement.querySelectorAll(
+          "td .methods span"
+        );
+      const userName =
+        item.parentElement.parentElement.parentElement.querySelector(
+          "td .card-info1 h3"
+        );
+      const wallet = item.parentElement.parentElement.parentElement.querySelector(
+        "td .card-info2 .wallet span"
+      );
+      const purchaseAmnt = document.querySelector(
+        ".payment-method-bank #amount input"
+      );
+      const proceedBtn = document.querySelector(
+        ".payment-method-bank #proceed a"
+      );
+      const rate = item.parentElement.parentElement.parentElement.querySelector(
+        "td .card-info2 .wallet"
+      ).childNodes[0].textContent *1;
+      // console.log(rate)
+      
+
+      //Updating the amount to receive as the input value is typed
+      purchaseAmnt.addEventListener('input',()=>{
+        const orderCost = (purchaseAmnt.value * 1)*rate;
+        const transactionFee = (purchaseAmnt.value * 1)*rate - ((purchaseAmnt.value * 1)*rate - (purchaseAmnt.value * 1)*rate *0.01);
+        const receiveAmnt = (purchaseAmnt.value * 1)*rate - (purchaseAmnt.value * 1)*rate *0.01
+        document.querySelector('#floating_container .order_cost p').innerHTML = orderCost;
+        document.querySelector('#floating_container .fee p').innerHTML = transactionFee;
+        document.querySelector('#floating_container .receive p').innerHTML = receiveAmnt;
+      })
+
+
 
       //mapping the available payment options to the modal boxes
 
-
-      //NOTE 
+      //NOTE
       /*Will remove modal box from its current position and replace some elements
       inside it with the dynamic element and insert it back into the dom because its current position is not good
       */
+     const style = {
+        textAlign: 'center',
+        border: '2px solid red'
+     }
       var paymentModal = document
-      .querySelector('#floating_container')
-      .querySelectorAll('span');
-      paymentModal[0].innerHTML = `${paymentMethod1[0].innerHTML} | `;
-      paymentModal[1].innerHTML = `${paymentMethod1[1].innerHTML} | `;
-      paymentModal[2].innerHTML = paymentMethod1[2].innerHTML;
+        .querySelector("#floating_container")
+        .querySelector(".sp p");
+        // paymentModal.style.fontWeight='800';
+        // paymentModal.style.textAlign = 'center';
+      paymentModal.innerHTML += `<span class="one">${paymentMethod1[0].innerHTML}</span> `;
+      paymentModal.innerHTML += `<span class="one">${paymentMethod1[1].innerHTML}</span> `;
+      paymentModal.innerHTML += `<span class="one">${paymentMethod1[2].innerHTML}</span>`;
       
 
-      //Setting the name and id 
-      document.querySelector('#floating_container h3').innerHTML = `${userName.innerHTML} `;
-      document.querySelector('#floating_container .payment_receiving_methods input').value = wallet.innerHTML;
-      console.log(document.querySelector('#floating_container'));
-      
-      console.log(paymentMethod1[0].innerHTML,paymentMethod1[1].innerHTML,paymentMethod1[2].innerHTML);
-      console.log(userName);
-      console.log(paymentModal);
-      // console.log(purchaseAmnt);
-      console.log(proceedBtn);
+      //Setting the name and id
+      document.querySelector(
+        "#floating_container h3"
+      ).innerHTML = `${userName.innerHTML} `;
+      document.querySelector(
+        "#floating_container .payment_receiving_methods input"
+      ).value = wallet.innerHTML;
+
+      const userToDbID = document.querySelector(".payment_receiving_methods input[name='user_id']") ;
+      userToDbID.value = userID.value * 1;
+      const exchngRate = document.querySelector('#floating_container .rate p');
+      exchngRate.innerHTML = rate*1;
+      console.log(exchngRate);
 
       //NOTE
       /* as this button is The user id will be taken and sent to the database to get the addresses of the user's available payment methods and be returned
@@ -167,45 +222,43 @@ function loadData(method, url) {
       /* Some datas will be taken and set as the value of a hidden input element
       and when the form is submitted those data will be sent to the database and retrieved back to finish the form process*/
 
-      proceedBtn.addEventListener('click',(e)=>{
+      //The proceed button inside the payment modal
+      proceedBtn.addEventListener("click", (e) => {
         // e.stopPropagation();
         e.preventDefault();
-        // if(e.target.tagName === "INPUT"){
-        //   console.log(purchaseAmnt)
-        //   e.preventDefault();
-        // }
-        console.log('Confirm button worked',purchaseAmnt.value*1)
-        console.log(userID.value *1);
-      })
-
-    }else{
-      console.log('Not a button')
+        console.log("Confirm button worked", (purchaseAmnt.value * 1)*rate);
+        console.log(userID.value * 1,userToDbID.value*1);
+      });
+    } else {
+      console.log("Not a button");
     }
   }
   /*When this container is clicked, it will look for an event that has a button name and if its found then the handleclick() function will run
   based on what the code inside it and get values pertaining to that specific container alone*/
-  buy_tableBody.addEventListener('click',(e)=>{
+  buy_tableBody.addEventListener("click", (e) => {
     e.stopPropagation();
-      //The handle click event is what will be mapping real time data to the modal boxes dynamically
-      handleclick(e);
-      //The floating container button
-      const floatingContainer = document.querySelector('#floating_container');
-      const popUpContainer = document.querySelector('.popup-container4');
-      
-      console.log(popUpContainer);
-      //displaying the pop up container that the values will be inputed in
-      displayPopup4();
-  })
-////////////////////////////////////
+    //The handle click event is what will be mapping real time data to the modal boxes dynamically
+    handleclick(e);
+    //The floating container button
+    const floatingContainer = document.querySelector("#floating_container");
+    const popUpContainer = document.querySelector(".popup-container4");
+
+    console.log(popUpContainer);
+    //displaying the pop up container that the values will be inputed in
+    displayPopup4();
+  });
+  ////////////////////////////////////
   xhr.send();
 }
 ///////////////BUY POST/////////////////////
 const method_buy = "POST",
   url_buy = "../php/order_buy.php";
 loadData(method_buy, url_buy);
-
-
 /////////////////////////////////////
+
+
+
+
 
 function loadData2(method, url) {
   const xhr = new XMLHttpRequest();
@@ -269,111 +322,9 @@ const method_sell = "POST",
   url_sell = "../php/order_sell.php";
 loadData2(method_sell, url_sell);
 
-console.log(userID)
-`              <div class="transaction">
-<div class="payment-method-head">
-  <H4>OTHER PAYMENT METHODS</H4>
+// console.log(retrieved)
+const session = fetchAsync("../php/payment-options.php")
 
-  <div class="showbox-btn" style="background-color: var(--input); margin: .7rem 0;">
-    <button class="pick-btn active" id="bank-box-btn" style="background: transparent;">BANK</button>
-    <button class="pick-btn " id="chipper-box-btn" style="background: transparent;">CHIPPER</button>
-    <button class="pick-btn" id="pi-box-btn" style="background: transparent;">PI NETWORK</button>
-  </div>
-</div>
-<div class="payment-method-bank" id="bank-box">
-  <div class="name-details">
-    <h4>BANK NAME:</h4>
-    <p>SPRING BANK</p>
-  </div>
-  <div class="name-details">
-    <h4>ACCNT NAME:</h4>
-    <p>ADEJUYI SAMSON J</p>
-  </div>
-  <div class="name-details">
-    <h4>ACCOUNT NO:</h4>
-    <p>2187093564</p>
-  </div>
-  <div class="name-details">
-    <h4>PURCHASE AMOUNT</h4><input type="tel" placeholder=" $">
-  </div>
-  <div class="name-details">
-    <h4>PURCHASE COST:</h4>
-    <p>&#x20A6;74,000</p>
-  </div>
-  <div class="name-details">
-    <h4>FEE 1%:</h4>
-    <p>$1</p>
-  </div>
-  <div class="name-details">
-    <h4>RECEIVE AMOUNT:</h4>
-    <p>$99.00</p>
-  </div>
-  <div class="btn-proceed">
+sessionStorage.setItem('session',session)
+console.log(sessionStorage.getItem('session'));
 
-    <a href="verify.html">
-      <button class="close-btn4" id="cancel-inner"> CONFIRM</button>
-    </a>
-
-    <button class="close-btn5" id="cancel" style="background-color: var(--secondary);">CANCEL</button>
-  </div>
-</div>
-<div class="pi-box" id="pi-box" style="display: none;">
-
-  <div class="name-details">
-    <h4>METHOD NAME:</h4>
-    <p>PI NETWORK</p>
-  </div>
-  <div class="name-details">
-    <h4>WALLET ADDRESS:</h4>
-    <p>#2187093564</p>
-  </div>
-  <div class="name-details">
-    <h4>PURCHASE AMOUNT</h4> <input type="tel" placeholder=" $">
-  </div>
-  <div class="name-details">
-    <h4>PURCHASE COST:</h4>
-    <p>$100</p>
-  </div>
-  <div class="name-details">
-    <h4>FEE 1%:</h4>
-    <p>$1</p>
-  </div>
-  <div class="name-details">
-    <h4>RECEIVE AMOUNT:</h4>
-    <p>$99.00</p>
-  </div>
-  <div class="btn-proceed">
-    <button class="popup-confirm4">CONFIRM</button>
-    <button class="close-btn4" id="cancel" style="background-color: var(--secondary);">CANCEL</button>
-  </div>
-</div>
-<div class="chipper-box" id="chipper-box" style="display: none;">
-  <div class="name-details">
-    <h4>METHOD NAME:</h4>
-    <p>CHIPPER CASH</p>
-  </div>
-  <div class="name-details">
-    <h4> USERNAME:</h4>
-    <p>@omotayocash</p>
-  </div>
-  <div class="name-details">
-    <h4>PURCHASE AMOUNT</h4> <input type="tel" placeholder=" $">
-  </div>
-  <div class="name-details">
-    <h4>PURCHASE COST:</h4>
-    <p>&#x20A6;74,000</p>
-  </div>
-  <div class="name-details">
-    <h4>FEE 1%:</h4>
-    <p>$1</p>
-  </div>
-  <div class="name-details">
-    <h4>RECEIVE AMOUNT:</h4>
-    <p>$99.00</p>
-  </div>
-  <div class="btn-proceed">
-    <button class="popup-confirm4">CONFIRM</button>
-    <button class="close-btn4" id="cancel" style="background-color: var(--secondary);">CANCEL</button>
-  </div>
-</div>
-</div>`
